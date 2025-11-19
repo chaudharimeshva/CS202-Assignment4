2835f2a9e42e6904ef4940a11bd826ccc69ab5bf
chaudharimeshva <167230299+chaudharimeshva@users.noreply.github.com>
2025-11-19 16:01:28 +0530

    Update Form1.Designer.cs

--- Lab_12/TaskA&TaskB/Form1.Designer.cs
+++ Lab_12/TaskA&TaskB/Form1.Designer.cs
@@ -28,10 +28,8 @@
             this.comboBoxColors = new System.Windows.Forms.ComboBox();
 
             this.SuspendLayout();
-
-            // 
+            
             // btnChangeColor
-            // 
             this.btnChangeColor.Location = new System.Drawing.Point(30, 150);
             this.btnChangeColor.Name = "btnChangeColor";
             this.btnChangeColor.Size = new System.Drawing.Size(120, 35);
@@ -39,9 +37,7 @@
             this.btnChangeColor.UseVisualStyleBackColor = true;
             this.btnChangeColor.Click += new System.EventHandler(this.btnChangeColor_Click);
 
-            // 
             // btnChangeText
-            // 
             this.btnChangeText.Location = new System.Drawing.Point(180, 150);
             this.btnChangeText.Name = "btnChangeText";
             this.btnChangeText.Size = new System.Drawing.Size(120, 35);
@@ -49,9 +45,7 @@
             this.btnChangeText.UseVisualStyleBackColor = true;
             this.btnChangeText.Click += new System.EventHandler(this.btnChangeText_Click);
 
-            // 
             // lblDisplay
-            // 
             this.lblDisplay.AutoSize = true;
             this.lblDisplay.Font = new System.Drawing.Font("Arial", 14F);
             this.lblDisplay.Location = new System.Drawing.Point(30, 30);
@@ -59,18 +53,14 @@
             this.lblDisplay.Size = new System.Drawing.Size(224, 22);
             this.lblDisplay.Text = "Welcome to Events Lab";
 
-            // 
             // comboBoxColors
-            // 
             this.comboBoxColors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxColors.FormattingEnabled = true;
             this.comboBoxColors.Location = new System.Drawing.Point(30, 90);
             this.comboBoxColors.Name = "comboBoxColors";
             this.comboBoxColors.Size = new System.Drawing.Size(150, 23);
 
-            // 
             // Form1
-            // 
             this.ClientSize = new System.Drawing.Size(350, 250);
             this.Controls.Add(this.btnChangeColor);
             this.Controls.Add(this.btnChangeText);
@@ -85,3 +75,4 @@
         #endregion
     }
 }
+
