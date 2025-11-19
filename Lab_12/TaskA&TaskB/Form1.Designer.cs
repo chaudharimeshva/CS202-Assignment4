765e9ed76c9ab7baaf0fa5306b0dfe7b7f4fb834
chaudharimeshva <167230299+chaudharimeshva@users.noreply.github.com>
2025-11-19 15:50:10 +0530

    Add files via upload

---
+++ Lab_12/TaskA&TaskB/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace EventPlayground
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Button btnChangeColor;
+        private System.Windows.Forms.Button btnChangeText;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.ComboBox comboBoxColors;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnChangeColor = new System.Windows.Forms.Button();
+            this.btnChangeText = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.comboBoxColors = new System.Windows.Forms.ComboBox();
+
+            this.SuspendLayout();
+
+            // 
+            // btnChangeColor
+            // 
+            this.btnChangeColor.Location = new System.Drawing.Point(30, 150);
+            this.btnChangeColor.Name = "btnChangeColor";
+            this.btnChangeColor.Size = new System.Drawing.Size(120, 35);
+            this.btnChangeColor.Text = "Change Color";
+            this.btnChangeColor.UseVisualStyleBackColor = true;
+            this.btnChangeColor.Click += new System.EventHandler(this.btnChangeColor_Click);
+
+            // 
+            // btnChangeText
+            // 
+            this.btnChangeText.Location = new System.Drawing.Point(180, 150);
+            this.btnChangeText.Name = "btnChangeText";
+            this.btnChangeText.Size = new System.Drawing.Size(120, 35);
+            this.btnChangeText.Text = "Change Text";
+            this.btnChangeText.UseVisualStyleBackColor = true;
+            this.btnChangeText.Click += new System.EventHandler(this.btnChangeText_Click);
+
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Arial", 14F);
+            this.lblDisplay.Location = new System.Drawing.Point(30, 30);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(224, 22);
+            this.lblDisplay.Text = "Welcome to Events Lab";
+
+            // 
+            // comboBoxColors
+            // 
+            this.comboBoxColors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxColors.FormattingEnabled = true;
+            this.comboBoxColors.Location = new System.Drawing.Point(30, 90);
+            this.comboBoxColors.Name = "comboBoxColors";
+            this.comboBoxColors.Size = new System.Drawing.Size(150, 23);
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(350, 250);
+            this.Controls.Add(this.btnChangeColor);
+            this.Controls.Add(this.btnChangeText);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.comboBoxColors);
+            this.Name = "Form1";
+            this.Text = "Event Playground";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+    }
+}
---
+++ Lab_12/TaskA&TaskB/Form1.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EventPlayground
+{
+    public partial class Form1 : Form
+    {
+        // ---------------------------------------------
+        //  CUSTOM DELEGATES USING CUSTOM EVENTARGS
+        // ---------------------------------------------
+        public delegate void ColorChangedHandler(object sender, ColorEventArgs e);
+        public delegate void TextChangedHandler(string newText);
+
+        // ---------------------------------------------
+        //  CUSTOM EVENTS
+        // ---------------------------------------------
+        public event ColorChangedHandler ColorChangedEvent;
+        public event TextChangedHandler TextChangedEvent;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Add color options
+            comboBoxColors.Items.Add("Red");
+            comboBoxColors.Items.Add("Green");
+            comboBoxColors.Items.Add("Blue");
+            comboBoxColors.SelectedIndex = 0;
+
+            // MULTIPLE SUBSCRIBERS FOR COLOR EVENT
+            ColorChangedEvent += UpdateLabelColor;
+            ColorChangedEvent += ShowNotification;
+
+            // Subscriber for Text Event
+            TextChangedEvent += OnTextChanged;
+        }
+
+        // ----------------------------------------------------
+        // Subscriber Method 1 ? Update Label Color
+        // ----------------------------------------------------
+        private void UpdateLabelColor(object sender, ColorEventArgs e)
+        {
+            Color selected = Color.Black;
+
+            if (e.ColorName == "Red") selected = Color.Red;
+            if (e.ColorName == "Green") selected = Color.Green;
+            if (e.ColorName == "Blue") selected = Color.Blue;
+
+            lblDisplay.ForeColor = selected;
+        }
+
+        // ----------------------------------------------------
+        // Subscriber Method 2 ? Show Notification
+        // ----------------------------------------------------
+        private void ShowNotification(object sender, ColorEventArgs e)
+        {
+            MessageBox.Show("Selected Color: " + e.ColorName);
+        }
+
+        // ----------------------------------------------------
+        // Text Changed Subscriber
+        // ----------------------------------------------------
+        private void OnTextChanged(string newText)
+        {
+            lblDisplay.Text = newText;
+        }
+
+        // ----------------------------------------------------
+        // Button ? Fires ColorChangedEvent
+        // ----------------------------------------------------
+        private void btnChangeColor_Click(object sender, EventArgs e)
+        {
+            string selectedColor = comboBoxColors.SelectedItem.ToString();
+
+            // FIRE THE EVENT with ColorEventArgs
+            ColorChangedEvent?.Invoke(this, new ColorEventArgs(selectedColor));
+        }
+
+        // ----------------------------------------------------
+        // Button ? Fires TextChangedEvent
+        // ----------------------------------------------------
+        private void btnChangeText_Click(object sender, EventArgs e)
+        {
+            string time = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+
+            TextChangedEvent?.Invoke("Updated: " + time);
+        }
+    }
+
+    // ================================================================
+    //                CUSTOM EVENTARGS CLASS
+    // ================================================================
+    public class ColorEventArgs : EventArgs
+    {
+        public string ColorName { get; }
+
+        public ColorEventArgs(string colorName)
+        {
+            ColorName = colorName;
+        }
+    }
+}
